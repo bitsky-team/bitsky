f1daf982344e150ba1ff9c8a8c18838caf73a033
Jason Van Malder <jasonvanmalder@gmail.com>
2019-07-23 20:04:03 +0200

    Desktop bootstrapping: Redux & tslint

---
+++ src/desktop/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export const App = (): JSX.Element => {
+  return (
+    <div className='App'>
+      <h1>This shit is working !</h1>
+    </div>
+  )
+}
---
+++ src/desktop/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+import { App } from './App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
+
+serviceWorker.unregister()
